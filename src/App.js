cb72103a07264af4a8124084e4f843fe0faf50cf
Hang Bui <hangbui6565@gmail.com>
2024-05-20 22:39:12 +0700

    View manager

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as  Route, Routes } from 'react-router-dom';
 import Sidebar from './Componnents/Sidebar/Sidebar';
 import Profile from './Componnents/Profile/Profile';
 // import LoginForm from './Componnents/LoginForm/LoginForm'
+// import ManagerList from './Componnents/Management/ManagerList';
+import ManagerList from './Componnents/Management/ManagerList';
 
 function App() {
     return (
-       
-            <Sidebar>
-                <Routes>                 
-                    <Route path="/profile" element={<Profile />} />                 
-                </Routes>
-            </Sidebar>
-            
+    //    <Profile />
+    //         <Sidebar>
+    //             <Routes>                 
+    //                 <Route path="/profile" element={<Profile />} />                 
+    //             </Routes>
+    //         </Sidebar>
+            <ManagerList />
     );
 }
 
