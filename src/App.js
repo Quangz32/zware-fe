50860328a846c3a1598df965d5f0ba9b1f234cd3
Stealer0 <duongthanhlong2004@gmail.com>
2024-05-25 23:59:26 +0700

    fix Sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { BrowserRouter as  Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './Componnents/Sidebar/Sidebar';
 import Profile from './Componnents/Profile/Profile';
-// import LoginForm from './Componnents/LoginForm/LoginForm'
-// import ManagerList from './Componnents/Management/ManagerList';
-import ManagerList from './Componnents/Management/ManagerList';
 import Home from './Componnents/Home/Home';
+// import Item from './Componennts/Item/Item';
+// import Report from './Componennts/Report/Report';
+// import Transaction from './Componennts/Transaction/Transaction';
+import Login from './Componnents/LoginForm/LoginForm';
 
 function App() {
     return (
-    <Sidebar>
-      <Home/>
-    </Sidebar>
-
+        <Router>
+            <div >
+                <Sidebar />
+                <main className="main">
+                    <Routes>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/profile" element={<Profile />} />
+                        {/* <Route path="/item" element={<Item />} />
+                        <Route path="/report" element={<Report />} />
+                        <Route path="/transaction" element={<Transaction />} /> */}
+                        <Route path="/login" element={<Login />} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     );
 }
 
