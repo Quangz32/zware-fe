886fa2e721bf87fb55a730f37ec68744fa401d31
Bui Hang <hangbui6565@gmail.com>
2024-05-28 00:01:01 +0700

    update Product

--- src/App.js
+++ src/App.js
@@ -4,32 +4,46 @@ import Sidebar from './Componnents/Sidebar/Sidebar';
 import Profile from './Componnents/Profile/Profile';
 // import LoginForm from './Componnents/LoginForm/LoginForm'
 // import ManagerList from './Componnents/Management/ManagerList';
-import ManagerList from './Componnents/Management/ManagerList';
 // import AdminPanel from './Componnents/Management/AdminPanel';
 // import Product from './Componnents/Product/Product';
 // import AddProduct from './Componnents/Product/AddProduct';
-import InventoryOutput from './InventoryOutput/InventoryOutput';
 import ProductList from './Componnents/Product/ProductList';
-import WarehouseManagerment from './Componnents/WarehouseManagement/WarehouseManagement';
-function App() {
-    return (
-    //    <Profile />
-            // <Sidebar>
-            //     <Routes>                 
-            //         <Route path="/profile" element={<Profile />} />                 
-            //     </Routes>
-            // </Sidebar>
+// import WarehouseManagerment from './Componnents/WarehouseManagement/WarehouseManagement';
+// function App() {
+//     return (
+//     //    <Profile />
+//             // <Sidebar>
+//             //     <Routes>                 
+//             //         <Route path="/profile" element={<Profile />} />                 
+//             //     </Routes>
+//             // </Sidebar>
            
-            <div className="app">
-            {/* <Product /> */}
-            <ProductList />
-      {/* <InventoryOutput /> */}
-      {/* <ManagerList /> */}
-{/* <WarehouseManagerment />   */}
-          </div>
+//             <div className="app">
+//             {/* <Product /> */}
+//             <ProductList />
+//       {/* <InventoryOutput /> */}
+//       {/* <ManagerList /> */}
+// {/* <WarehouseManagerment />   */}
+//           </div>
     
     
+//     );
+// }
+const App = () => {
+    return (
+      <Router>
+        <div className="container">
+          <Sidebar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/products" component={ProductList} />
+          </Switch>
+        </div>
+      </Router>
     );
-}
-
+  };
+  
+  const Home = () => {
+    return <h2>Home Page</h2>;
+  };
 export default App;
